8d575a773d53ca6b00d543c6a357d02333b984a2
EvannFf <evancollege789@gmail.com>
2021-05-12 20:13:02 -0400

    Figured out how to populate the database!!! - EF

--- populateDatabase.py
+++ populateDatabase.py
@@ -2,6 +2,11 @@ import os
 import requests
 import urllib.parse
 from pprint import pprint
+import re
+from cs50 import SQL
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///project.db")
 
 
 def lookup(IMDBID):
@@ -20,30 +25,35 @@ def lookup(IMDBID):
     # Parse response
     try:
         data = response.json()
-        print(response.status_code)
+        # ensure language is English
+        if data["original_language"] != "en":
+            return None
+        # print(response.status_code)
         # pprint(data["release_dates"]["results"])
-        rating = "placeholder"
 
+        # handle Rating
         # https://stackoverflow.com/a/4391978/6568837
+        rating = ""
         for i, dic in enumerate(data["release_dates"]["results"]):
             if dic["iso_3166_1"] == "US":
                 rating = data["release_dates"]["results"][i]["release_dates"][0]["certification"]
 
+        # handle Genres
         genres = []
         for i in range(len(data["genres"])):
             genres.append(data["genres"][i]["name"])
+        gString = ", ".join(genres)
 
         return {
             "imdb_id": data["imdb_id"],
             "tmdb_id": data["id"],
             "title": data["title"],
-            "release_date": data["release_date"],
+            "release_year": int(data["release_date"][0:4]),
             "runtime": data["runtime"],
             "vote_average": data["vote_average"],
             "vote_count": data["vote_count"],
-            "genres": data["genres"],
             "rating": rating,
-            "genres": genres
+            "genres": gString
         }
     except (KeyError):
         return "KeyError"
@@ -52,4 +62,29 @@ def lookup(IMDBID):
     except (ValueError):
         return "ValueError"
 
-print(lookup("tt0451279"))
+
+def increment(ID):
+    """Increment ID by 1"""
+
+    # https://stackoverflow.com/a/67505817/6568837
+    newID = f'tt{int(ID.lstrip("t"))+1:07}'
+    return newID
+
+
+def populate():
+    """populate database"""
+
+    ID = "tt1000000"
+    for i in range(100):
+        data = lookup(ID)
+        if data != None:
+            try:
+                # Input data into database
+                db.execute("INSERT INTO movie_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", data["imdb_id"], data["tmdb_id"], data["title"], data["release_year"], data["runtime"], data["vote_average"], data["vote_count"], data["genres"], data["rating"])
+            except (ValueError):
+                continue
+        ID = increment(ID)
+
+
+#print(lookup("tt1000093"))
+populate()
